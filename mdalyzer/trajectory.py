0fc740fcd42f7361b2a025f83fe6617878e1f572
apsantos <apsantos@princeton.edu>
2014-12-18 17:06:36 -0500

    Add dcd class; create a pointer to another trajectory type for initial configuration and position data; check that user asks for a compatible file type

--- mdalyzer/trajectory.py
+++ mdalyzer/trajectory.py
@@ -24,9 +24,54 @@ class hoomd(trajectory):
             self.cpp.addFile(file)
             
 class dcd(trajectory):
-    """DCD trajectory object"""
-    def __init__(self):
-        self.cpp = libmdalyzer.DCDTrajectory()
+    """DCD trajectory object
+       Make sure that all other trajectory classes preceed the dcd class, 
+       as it depends on all others
+    """
+    def __init__(self, dcd_file, i_file, i_type=None):
+        self.traj_types = {'HOOMDXML':libmdalyzer.HOOMDXMLTrajectory(), 'XML':libmdalyzer.HOOMDXMLTrajectory()}
+        self.dcd_file = dcd_file
+        self.i_file = i_file
+        self.i_type = i_type
+        self.i_file_ptr = trajectory()
+        if (self.i_type != None):
+            if ( self.traj_types.has_key(self.i_type) ):
+                # loop over the avilable formats
+                for t_type in self.traj_types:
+                    if ( t_type.lower() == self.i_type.lower() ):
+                        # create pointer to that trajectory
+                        self.i_file_ptr.cpp = self.traj_types[t_type]
+                        # add the file frame to that pointer
+                        self.i_file_ptr.cpp.addFile(i_file)
+
+            # if type is incompatible   
+            else:
+                self._notAtype()
+
+        else:
+            extension = self.i_file.split('.')
+            for i in extension:
+                if ( self.traj_types.has_key(extension[i]) ):
+                    # loop over the avilable formats
+                    for t_type in self.traj_types:
+                        if ( t_type.lower() == self.i_type.lower() ):
+                            # create pointer to that trajectory
+                            self.i_file_ptr.cpp = self.traj_types[t_type]
+                            # add the file frame to that pointer
+                            self.i_file_ptr.cpp.addFile(i_file)
+
+                # if type is incompatible   
+                else:
+                    self._notAtype()
+
+        self.cpp = libmdalyzer.DCDTrajectory(self.i_file_ptr, self.dcd_file)
+
+    def _notAtype(self) :
+        err_str = ('The file type \'%s\' does not exist. Maybe you meant one of these:\n' % self.i_file)
+        for t_type in self.traj_types:
+            err_str += (t_type + " ")
+        raise RunTimeError(err_str)
+        return
         
     def add(self, files):
         """Add frames by file name"""

6c19f2bb59a850dd2b2eaa67b34c66e2f22c17bc
Michael Howard <mphoward@princeton.edu>
2014-12-29 13:55:43 -0500

    Add documentation to Frame and TriclinicBox * Change Frame memory allocation to only allocate on first access

--- libmdalyzer/data_structures/TriclinicBox.cc
+++ libmdalyzer/data_structures/TriclinicBox.cc
@@ -1,22 +1,39 @@
+/*!
+ * \file TriclinicBox.cc
+ * \author Michael P. Howard
+ * \date 29 December 2014
+ * \brief Implementation of TriclinicBox data structure
+ */
 #include "TriclinicBox.h"
 #include <cmath>
 #include <boost/python.hpp>
 
 TriclinicBox::TriclinicBox()
-    : m_length(Vector3<double>()), m_tilt(Vector3<double>())
-    {}
-    
-TriclinicBox::TriclinicBox(Vector3<double> length)
-    : m_length(length), m_tilt(Vector3<double>())
+    : m_length(Vector3<double>(0.0,0.0,0.0)), m_tilt(Vector3<double>(0.0,0.0,0.0))
     {}
 
+/*!
+ * \param length box edge lengths
+ */   
+TriclinicBox::TriclinicBox(Vector3<double> length)
+    : m_length(length), m_tilt(Vector3<double>(0.0,0.0,0.0))
+    {}
+
+/*!
+ * \param length box edge lengths
+ * \param tilt box tilt factors (dimensionless)
+ */
 TriclinicBox::TriclinicBox(Vector3<double> length, Vector3<double> tilt)
     : m_length(length), m_tilt(tilt)
     {}
-    
-//! construct a box from three arbitrarily oriented vectors
+
 /*!
- * Formulae come from the HOOMD-blue documentation https://codeblue.umich.edu/hoomd-blue/doc/page_box.html
+ * \param v1 a lattice vector
+ * \param v2 b lattice vector
+ * \param v3 c lattice vector
+ *
+ * Formulae come from the
+ * <a href="https://codeblue.umich.edu/hoomd-blue/doc/page_box.html">HOOMD-blue periodic boundary documentation</a>.
  */
 TriclinicBox::TriclinicBox(Vector3<double> v1, Vector3<double> v2, Vector3<double> v3)
     {
@@ -39,6 +56,20 @@ TriclinicBox::TriclinicBox(Vector3<double> v1, Vector3<double> v2, Vector3<doubl
     double a3x = v1.dot(v3)/m_length.x;
     m_tilt.y = a3x/m_length.z; //xz
     m_tilt.z = (v2.dot(v3)-a2x*a3x)/(m_length.y*m_length.z);
+    }
+
+/*!
+ * \param image 3d vector with counts of times the particle moved through the box
+ * \param pos periodically wrapped image
+ *
+ * Formula taken from
+ * <a href="https://codeblue.umich.edu/hoomd-blue/doc/page_xml_file_format.html#sec_xml_image">HOOMD-blue documentation for images</a>.
+ */   
+void TriclinicBox::shiftImage(const Vector3<double>& image, Vector3<double>& pos) const
+    {
+    pos.x += image.x * m_length.x + m_tilt.x * image.y * m_length.y + m_tilt.y * image.z * m_length.z;
+    pos.y += image.y * m_length.y + m_tilt.z * image.z * m_length.z;
+    pos.z += image.z * m_length.z;
     }
 
 void export_TriclinicBox()

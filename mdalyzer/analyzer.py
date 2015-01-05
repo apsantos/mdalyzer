4a39e826e3f23c7ce261b3093fb8eeb265db72e0
Michael Howard <mphoward@princeton.edu>
2015-01-05 16:37:40 -0500

    Add TemperatureProfile analyzer * Remove unused variable from DensityProfile

--- mdalyzer/analyzer.py
+++ mdalyzer/analyzer.py
@@ -57,6 +57,38 @@ class density(analyzer):
         self.weight = weight
         self.cpp.useMassWeighting(self.weight)
     
+class temperature(analyzer):
+    """Temperature profile analyzer"""
+    
+    def __init__(self, traj, file_name='temperature', nx=0, ny=0, nz=0, name=None, types=[]):
+        analyzer.__init__(self, traj, file_name, name)
+        self.bins = libmdalyzer.Vector3uint(nx,ny,nz)
+        
+        self.cpp = libmdalyzer.TemperatureProfile(self.trajectory.cpp, self.file_name, self.bins)
+        self.trajectory.cpp.addCompute(self.cpp, self.name)
+        
+        self.types = []
+        self.add_type(types)
+    
+    def add_type(self, types):
+        """Add types to calculate"""
+        if not isinstance(types, (list,tuple)):
+            types = [types]
+            
+        for t in types:
+            if t not in self.types:
+                self.types += [t]
+                self.cpp.addType(t)
+    
+    def delete_type(self, types):
+        """Remove types to calculate"""
+        if not isinstance(types, (list,tuple)):
+            types = [types]
+            
+        for t in types:
+            self.types.remove(t)
+            self.cpp.deleteType(t)   
+
 class clustering(analyzer):
     """Clustering analysis"""
     def __init__(self, traj, file_name='clustering.dat', distance=1.0, name=None):

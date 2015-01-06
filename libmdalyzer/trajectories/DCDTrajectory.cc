7fb5fcb710b9ec402d5b05428449dbbf7274f602
apsantos <apsantos@princeton.edu>
2015-01-06 08:51:26 -0500

    Add the dcd output frequency and timestep as a user-defined input; default to the DCD file values; Add more doxygen comments.

--- libmdalyzer/trajectories/DCDTrajectory.cc
+++ libmdalyzer/trajectories/DCDTrajectory.cc
@@ -12,10 +12,16 @@
 #include <sstream>
 #include <fstream>
 
-/*! \param file Path to DCD file to read
+/*! Constructor 
+ *  \param initial_traj Pointer to the initial trajectory of another file type
+ *  \param fileName Path to DCD file to read
+ *  \param time_step Number of time units between each step 
+ *  \param dcd_freq Number of timesteps between each dcd output configuration
  */
-DCDTrajectory::DCDTrajectory( boost::shared_ptr<Trajectory> initial_traj, const std::string& fileName)
-    : m_initial_traj(initial_traj), m_file(fileName)
+DCDTrajectory::DCDTrajectory( boost::shared_ptr<Trajectory> initial_traj, 
+                      const std::string& fileName, double time_step, unsigned int dcd_freq)
+    : m_initial_traj(initial_traj), m_file(fileName), 
+      m_time_step(time_step), m_dcd_freq(dcd_freq)
     {
     }
 
@@ -29,15 +35,38 @@ void DCDTrajectory::readHeader(FILE* fileptr)
         
     int errcode;
     int frame_skip_c;             //!< frame read interval
+    int frame_start_c;            //!< starting frame for reading
+    int n_frames_c;               //!< num of frames in dcd file
+    double dcd_ts;                //!< timestep in the dcd
 
-    errcode = read_dcdheader(fileptr, &m_n_dcdparticles_c, &m_n_frames_c, &m_frame_start_c,
-                            &frame_skip_c, &m_time_step, &m_n_fixed_c, &m_freeparticles_c,
+    errcode = read_dcdheader(fileptr, &m_n_dcdparticles_c, &n_frames_c, &frame_start_c,
+                            &frame_skip_c, &dcd_ts, &m_n_fixed_c, &m_freeparticles_c,
                             &m_reverse_endian_c, &m_charmm_flags_c);
 
     m_n_dcdparticles = (unsigned int)m_n_dcdparticles_c;
-    m_n_frames = (unsigned int)m_n_frames_c;
-    m_frame_start = (unsigned int)m_frame_start_c;
+    m_n_frames = (unsigned int)n_frames_c;
+    m_frame_start = (unsigned int)frame_start_c;
+    //! Check if the user input values match the dcd files
+    //! Use output dcd frequency and timestep from DCD file if user did not
+    if (m_dcd_freq < 0.001)
+        {
+        m_dcd_freq = (unsigned int)frame_skip_c;
+        }
+    else if (m_dcd_freq != (unsigned int)frame_skip_c)
+        {
+        // notify the user
+        }
 
+    if (m_time_step < 0.001)
+        {
+        m_time_step = dcd_ts;
+        }
+    else if (m_time_step != (unsigned int)dcd_ts)
+        {
+        // notify the user
+        }
+
+    //! Check that the DCD has the correct format, and is not empty
     if (errcode < 0 || m_n_dcdparticles <= 0) 
         {
         if (errcode == DCD_BADFORMAT)
@@ -137,19 +166,18 @@ void DCDTrajectory::read()
     m_must_read_from_file = false;
     }
 
+//! Read a DCD file using an external parser
 void DCDTrajectory::readFromFile()
     {
 
     // open the file
     const char * c_filename = m_file.c_str();
     FILE* fileptr = open_dcd_read(c_filename);
-    // check that the DCD exists
+    //! check that the DCD exists
     if (fileptr == NULL)
         {
         throw std::runtime_error("ERROR: cannot find DCD file " +  m_file);
         }
-
-    // check that there is an initial frame 
 
     // read the DCD header
     readHeader(fileptr);
@@ -167,7 +195,7 @@ void DCDTrajectory::readFromFile()
     for (unsigned int frame_idx = 0; frame_idx < m_n_frames; ++frame_idx)
         {
         boost::shared_ptr<Frame> cur_frame = readTimeStep(fileptr);
-        cur_frame->setTime( (double)(m_time_step * (frame_idx+1)) + start_time );
+        cur_frame->setTime( (double)(m_time_step * (m_dcd_freq * (frame_idx+1)) ) + start_time );
         m_frames.push_back(cur_frame);
         }
 
@@ -181,5 +209,5 @@ void export_DCDTrajectory()
     {
     using namespace boost::python;
     class_<DCDTrajectory, boost::shared_ptr<DCDTrajectory>, bases<Trajectory>, boost::noncopyable >
-    ("DCDTrajectory", init< boost::shared_ptr<Trajectory>, const std::string& >());
+    ("DCDTrajectory", init< boost::shared_ptr<Trajectory>, const std::string&, double, unsigned int >());
     }

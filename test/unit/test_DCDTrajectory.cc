bd23e71fd25d78937a0c79def51e475c3c76d2cb
apsantos <apsantos@princeton.edu>
2015-01-06 08:53:03 -0500

    Test default values; Correct DCD_multi_frame test.

--- test/unit/test_DCDTrajectory.cc
+++ test/unit/test_DCDTrajectory.cc
@@ -12,8 +12,8 @@
 #include "TriclinicBox.h"
 #include "VectorMath.h"
 #include "Trajectory.h"
-#include "HOOMDXMLTrajectory.h"
 #include "DCDTrajectory.h"
+#include "HOOMDXMLTrajectory.h"
 
 #include <boost/shared_ptr.hpp>
 #include <vector>
@@ -21,95 +21,42 @@
 
 BOOST_AUTO_TEST_SUITE(DCD)
 //! test all information is read from a single frame
-BOOST_AUTO_TEST_CASE(HOOMDXML_read_frame)
+BOOST_AUTO_TEST_CASE(DCD_read_frame)
     {
-	HOOMDXMLTrajectory traj;
+	HOOMDXMLTrajectory traj(1.0);
 	traj.addFile("test/unit/hoomd/frame.xml.1");
 	traj.analyze();
 	
 	TrajectoryCheck config;
-	config.box = true; config.position = true; config.velocity = true;
+	config.position = true;
 	config.type = true; config.diameter = true; config.mass = true;
 	
 	test_read<HOOMDXMLTrajectory>(traj,config);
     }
 //! test all information is read from multiple frames
-BOOST_AUTO_TEST_CASE(HOOMDXML_read_multiframes)
+BOOST_AUTO_TEST_CASE(DCD_read_multiframes)
     {
-    HOOMDXMLTrajectory traj;
-    traj.addFile("test/unit/hoomd/frame.xml.1");
-    traj.addFile("test/unit/hoomd/frame.xml.2");
+	boost::shared_ptr<HOOMDXMLTrajectory> i_traj( new HOOMDXMLTrajectory(1.0) );
+    i_traj->addFile("test/unit/hoomd/frame.xml.1");
+	DCDTrajectory traj(i_traj, "test/unit/dcd/frame.dcd.2", 2.5, 1);
     traj.analyze();
     
     TrajectoryCheck config;
-	config.box = true; config.position = true; config.velocity = true;
-	config.type = true; config.diameter = true; config.mass = true;
+	config.position = true;
     
-    test_multiframe<HOOMDXMLTrajectory>(traj, config);
+    test_multiframe<DCDTrajectory>(traj, config);
     }
-//! test file can be read if positions are not set (special first field)
-BOOST_AUTO_TEST_CASE(HOOMDXML_read_only_velocity)
+/*! test that the DCD reader defaults to the output frequency and timestep to what is 
+ * in the DCD file, if the user specifies 0
+ */
+BOOST_AUTO_TEST_CASE(DCD_read_default)
     {
-    HOOMDXMLTrajectory traj;
-    traj.addFile("test/unit/hoomd/frame.xml.only_velocity");
+	boost::shared_ptr<HOOMDXMLTrajectory> i_traj( new HOOMDXMLTrajectory(1.0) );
+    i_traj->addFile("test/unit/hoomd/frame.xml.1");
+	DCDTrajectory traj(i_traj, "test/unit/dcd/frame.dcd.2", 0, 0);
     traj.analyze();
-
-    boost::shared_ptr<Frame> frame = traj.getFrame(0);    
-    BOOST_REQUIRE(frame->hasVelocities());
-        
-    std::vector< Vector3<double> > velocities = frame->getVelocities();
-    BOOST_CHECK_CLOSE(velocities[0].x, 3.0, test_very_close);
-    BOOST_CHECK_CLOSE(velocities[0].y, -2.0, test_very_close);
-    BOOST_CHECK_CLOSE(velocities[0].z, 1.0, test_very_close);
     
-    BOOST_CHECK_CLOSE(velocities[1].x, -1.0, test_very_close);
-    BOOST_CHECK_CLOSE(velocities[1].y, 2.0, test_very_close);
-    BOOST_CHECK_CLOSE(velocities[1].z, -3.0, test_very_close);
-    }
-//! test exceptions are thrown for badly formed xml
-BOOST_AUTO_TEST_CASE(HOOMDXML_exceptions)
-    {
-    BOOST_TEST_MESSAGE("Testing exception throws of HOOMDXMLTrajectory, expect 6 output messages");
-    // file not found
-        {
-        HOOMDXMLTrajectory traj;
-        traj.addFile("test/unit/hoomd/not.a.file.xml");    
-        BOOST_CHECK_THROW(traj.analyze(), std::exception);
-        }
-    
-    // version too old
-        {
-        HOOMDXMLTrajectory traj;
-        traj.addFile("test/unit/hoomd/frame.xml.version");
-        BOOST_CHECK_THROW(traj.analyze(), std::exception);
-        }
-    
-    // missing timestep
-        {
-        HOOMDXMLTrajectory traj;
-        traj.addFile("test/unit/hoomd/frame.xml.timestep");
-        BOOST_CHECK_THROW(traj.analyze(), std::exception);
-        }  
-    
-    // no box
-        {
-        HOOMDXMLTrajectory traj;
-        traj.addFile("test/unit/hoomd/frame.xml.nobox");
-        BOOST_CHECK_THROW(traj.analyze(), std::exception);
-        }  
-        
-    // missing box edge
-        {
-        HOOMDXMLTrajectory traj;
-        traj.addFile("test/unit/hoomd/frame.xml.box");
-        BOOST_CHECK_THROW(traj.analyze(), std::exception);
-        }
-    
-    // velocity mismatch
-        {
-        HOOMDXMLTrajectory traj;
-        traj.addFile("test/unit/hoomd/frame.xml.velocity");
-        BOOST_CHECK_THROW(traj.analyze(), std::exception);
-        } 
+    std::vector< boost::shared_ptr<Frame> > frames = traj.getFrames();
+    BOOST_CHECK_CLOSE(frames[1]->getTime(), 1.0, test_very_close);
     }
 BOOST_AUTO_TEST_SUITE_END()

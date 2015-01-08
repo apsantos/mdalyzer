038098afa185b0527a49b0ae97c3d215c79f15f1
jovella62890 <vella@princeton.edu>
2015-01-07 20:18:35 -0500

    Velocity Autocorrelation Function
    Velocity Autocorrelation Function

--- mdalyzer/analyzer.py
+++ mdalyzer/analyzer.py
@@ -24,14 +24,12 @@ class density(analyzer):
         self.types = []
         self.weight = True
         
-        if not isinstance(types, list):
-            types = [types]
         self.add_type(types)
         self.mass_weight(weight)
     
     def add_type(self, types):
         """Add types to calculate"""
-        if not isinstance(types, list):
+        if not isinstance(types, (list,tuple)):
             types = [types]
             
         for t in types:
@@ -41,7 +39,7 @@ class density(analyzer):
     
     def delete_type(self, types):
         """Remove types to calculate"""
-        if not isinstance(types, list):
+        if not isinstance(types, (list,tuple)):
             types = [types]
             
         for t in types:
@@ -59,4 +57,83 @@ class density(analyzer):
         self.weight = weight
         self.cpp.useMassWeighting(self.weight)
     
+class clustering(analyzer):
+    """Clustering analysis"""
+    def __init__(self, traj, file_name='clustering.dat', distance=1.0, name=None):
+        analyzer.__init__(self, traj, file_name, name)
+        
+        self.distance = distance
+        self.cpp = libmdalyzer.Clustering(self.trajectory.cpp, self.file_name, self.distance)
+        self.trajectory.cpp.addCompute(self.cpp, self.name)
+    
+    def set_distance(self, distance):
+        """Set interatomic distance defining particles in a cluster"""
+        self.distance = distance
+        self.cpp.setDistance(self.distance)
+
+class msd(analyzer):
+    """Density profile analyzer"""
+    
+    def __init__(self, traj, file_name='msd', origins=1, name=None, types=[]):
+        analyzer.__init__(self, traj, file_name, name)
+        self.origins = origins
+        
+        self.cpp = libmdalyzer.MeanSquaredDisplacement(self.trajectory.cpp, self.file_name, self.origins)
+        self.trajectory.cpp.addCompute(self.cpp, self.name)
+        
+        self.types = []
+        
+        self.add_type(types)
+    
+    def add_type(self, types):
+        """Add types to calculate"""
+        if not isinstance(types, (list,tuple)):
+            types = [types]
+            
+        for t in types:
+            if t not in self.types:
+                self.types += [t]
+                self.cpp.addType(t)
+    
+    def delete_type(self, types):
+        """Remove types to calculate"""
+        if not isinstance(types, (list,tuple)):
+            types = [types]
+            
+        for t in types:
+            self.types.remove(t)
+            self.cpp.deleteType(t)
+            
+class vacf(analyzer):
+    """Velocity autocorrelation function analyzer"""
+    
+    def __init__(self, traj, file_name='vacf', origins=1, name=None, types=[]):
+        analyzer.__init__(self, traj, file_name, name)
+        self.origins = origins
+        
+        self.cpp = libmdalyzer.VelocityAutocorrelation(self.trajectory.cpp, self.file_name, self.origins)
+        self.trajectory.cpp.addCompute(self.cpp, self.name)
+        
+        self.types = []
+        
+        self.add_type(types)
+    
+    def add_type(self, types):
+        """Add types to calculate"""
+        if not isinstance(types, (list,tuple)):
+            types = [types]
+            
+        for t in types:
+            if t not in self.types:
+                self.types += [t]
+                self.cpp.addType(t)
+    
+    def delete_type(self, types):
+        """Remove types to calculate"""
+        if not isinstance(types, (list,tuple)):
+            types = [types]
+            
+        for t in types:
+            self.types.remove(t)
+            self.cpp.deleteType(t)
     

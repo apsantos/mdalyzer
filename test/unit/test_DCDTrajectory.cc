55cfd0026486f6c01870ad1714df3d8efef65c90
apsantos <apsantos@princeton.edu>
2015-01-09 16:27:33 -0500

    Add tests for DCD to read all available initial configuration formats

--- test/unit/test_DCDTrajectory.cc
+++ test/unit/test_DCDTrajectory.cc
@@ -13,29 +13,58 @@
 #include "VectorMath.h"
 #include "Trajectory.h"
 #include "DCDTrajectory.h"
+#include "GROTrajectory.h"
+#include "PDBTrajectory.h"
+#include "XYZTrajectory.h"
 #include "HOOMDXMLTrajectory.h"
 
 #include <boost/shared_ptr.hpp>
 
 BOOST_AUTO_TEST_SUITE(DCD)
-//! test all information is read from a single frame
-BOOST_AUTO_TEST_CASE(read_frame)
+//! test all information is read from gro input
+BOOST_AUTO_TEST_CASE(read_gro)
     {
-	HOOMDXMLTrajectory traj(1.0);
-	traj.addFile("test/unit/hoomd/frame.xml.1");
-	traj.analyze();
-	
-	TrajectoryCheck config;
+	boost::shared_ptr<GROTrajectory> i_traj( new GROTrajectory(4) );
+    i_traj->addFile("test/unit/dcd/frame.gro.1");
+	DCDTrajectory traj(i_traj, "test/unit/dcd/frame.dcd.2", 2.5, 1);
+    traj.analyze();
+    
+    TrajectoryCheck config;
 	config.position = true;
-	config.type = true; config.diameter = true; config.mass = true;
-	
-	test_read<HOOMDXMLTrajectory>(traj,config);
+    
+    test_multiframe<DCDTrajectory>(traj, config);
     }
-//! test all information is read from multiple files
-BOOST_AUTO_TEST_CASE(read_multi_files)
+//! test all information is read from pdb input
+BOOST_AUTO_TEST_CASE(read_pdb)
+    {
+	boost::shared_ptr<PDBTrajectory> i_traj( new PDBTrajectory(1.0) );
+    i_traj->addFile("test/unit/dcd/frame.pdb.1");
+	DCDTrajectory traj(i_traj, "test/unit/dcd/frame.dcd.2", 2.5, 1);
+    traj.analyze();
+    
+    TrajectoryCheck config;
+	config.position = true;
+    
+    test_multiframe<DCDTrajectory>(traj, config);
+    }
+//! test all information is read from xyz input
+BOOST_AUTO_TEST_CASE(read_xyz)
+    {
+	boost::shared_ptr<XYZTrajectory> i_traj( new XYZTrajectory() );
+    i_traj->addFile("test/unit/dcd/frame.xyz.1");
+	DCDTrajectory traj(i_traj, "test/unit/dcd/frame.dcd.2", 2.5, 1);
+    traj.analyze();
+    
+    TrajectoryCheck config;
+	config.position = true;
+    
+    test_multiframe<DCDTrajectory>(traj, config);
+    }
+//! test all information is read from xml input
+BOOST_AUTO_TEST_CASE(read_xml)
     {
 	boost::shared_ptr<HOOMDXMLTrajectory> i_traj( new HOOMDXMLTrajectory(1.0) );
-    i_traj->addFile("test/unit/hoomd/frame.xml.1");
+    i_traj->addFile("test/unit/dcd/frame.xml.1");
 	DCDTrajectory traj(i_traj, "test/unit/dcd/frame.dcd.2", 2.5, 1);
     traj.analyze();
     
@@ -50,7 +79,7 @@ BOOST_AUTO_TEST_CASE(read_multi_files)
 BOOST_AUTO_TEST_CASE(DCD_read_default)
     {
 	boost::shared_ptr<HOOMDXMLTrajectory> i_traj( new HOOMDXMLTrajectory(1.0) );
-    i_traj->addFile("test/unit/hoomd/frame.xml.1");
+    i_traj->addFile("test/unit/dcd/frame.xml.1");
 	DCDTrajectory traj(i_traj, "test/unit/dcd/frame.dcd.2", 0, 0);
     traj.analyze();
     
